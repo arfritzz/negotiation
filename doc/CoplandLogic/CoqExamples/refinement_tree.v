d017e662030a9b8445fd8586c1cbe372ec000bde
Anna Fritz <42523108+arfritzz@users.noreply.github.com>
2022-01-31 12:16:18 -0600

    adding folder for Coq examples

---
+++ doc/CoplandLogic/CoqExamples/refinement_tree.v
@@ -0,0 +1,16 @@
+(* first attempt at a refinement tree *)
+
+(* What are the layers of the refinement tree? 
+
+target identity 
+      |
+      |
+system components 
+      |
+      |
+primitive operations 
+
+At first glance, maybe these are the layers. The question is what occurs with more complex measurements? What would then be the next layer? 
+
+*)
+
