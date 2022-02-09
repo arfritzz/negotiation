a925081e5fa8bb4beb8cf01022c655c61f337bb6
Anna Fritz <42523108+arfritzz@users.noreply.github.com>
2022-02-09 10:45:42 -0600

    adding folder for first attempt at negotiate function

---
+++ src/haskell_neg/negotiate.hs
@@ -0,0 +1,36 @@
+{- What do we need? 
+    - target ... this is who send the proposal. This will eventually have type place. 
+    - appraiser ... who sends request. This will also have type place. 
+    - privacy policy ... assume now that nothing violates privacy policy 
+    - selection policy ... assume now that the longest measurement is the best -}
+
+    
+{- for now, possible protocols include copying, signing, hashing. 
+   to make a more complex TERM type, check out `Term_Defs.hs in /haskell-am/copland-interp/extracted_src/Term_Defs.hs-}
+data Protocol = 
+      CPY
+    | SIG 
+    | HSH 
+
+
+data Negotiate = Negotiate {  request :: Protocol
+                            , proposal :: [Protocol]
+                            , choosen_protocol :: Maybe Protocol }
+
+
+
+{- this negotiation function will be the negotiation of the target's side. 
+
+    things we could do: 
+    - could make a map of request to possible protocols? 
+    -}
+neg_t :: request -> [Protocol]
+neg_t r = 
+    case r of {
+        _ -> [HSH]
+    }
+
+neg_a :: [Protocol] -> Maybe Protocol
+neg_a p = case p of 
+    [] -> Nothing 
+    (x:_) -> Just x 
